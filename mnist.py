f5d125009b344881f2f17b2b682bbbcba2a00093
Jose Cupertino Ruiz Vargas <jcruizva@hotmail.com>
2020-06-12 00:44:34 -0300

    Implement image uploader

--- mnist.py
+++ mnist.py
@@ -6,12 +6,19 @@
 
       flask run --host 0.0.0.0
 """
-
-from flask import Flask, render_template, url_for
+from flask import (
+        Flask, flash, redirect, render_template, request,
+        send_from_directory, url_for
+        )
 import os
 
 
 app = Flask(__name__)
+
+
+@app.route("/model")
+def model():
+    return "previsao do modelo"
 
 
 @app.route("/<name>")
@@ -23,10 +30,29 @@ def show(name):
     )
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        if "file" not in request.files:
+            flash("No file part")
+            return redirect(request.url)
+        file = request.files["file"]
+        if file.filename == "":
+            flash("No selected file")
+            return redirect(request.url)
+        if file:
+            filename = file.filename
+            file.save(os.path.join("static", "imgs", filename))
+            return redirect(url_for("uploaded_file", filename=filename))
     imgs = os.listdir(r"static\imgs")
     return render_template(
         "index.html",
         imgs=imgs
     )
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(
+            os.path.join("static", "imgs"), filename
+            )
