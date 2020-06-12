c26077ed5c2fbc4f24095989aa250928e5f18735
Jose Cupertino Ruiz Vargas <jcruizva@hotmail.com>
2020-06-12 16:44:58 -0300

    Allow to delete images

--- mnist.py
+++ mnist.py
@@ -6,19 +6,23 @@
 
       flask run --host 0.0.0.0
 """
-from flask import (
-        Flask, flash, redirect, render_template, request,
-        send_from_directory, url_for
-        )
+from flask import Flask, flash, redirect, render_template, request, url_for
+from joblib import load
+from skimage import io, util
+import numpy as np
 import os
 
 
 app = Flask(__name__)
 
 
-@app.route("/model")
-def model():
-    return "previsao do modelo"
+def model(name):
+    img_path = os.path.join("static", "imgs", name)
+    im = io.imread(img_path, as_gray=True)
+    clf = load(os.path.join("static", "models", "mnist_svm.joblib"))
+    X = np.reshape(util.invert(im), (1, im.size))
+    prediction = clf.predict(X)
+    return prediction[0]
 
 
 @app.route("/<name>")
@@ -28,6 +32,13 @@ def show(name):
         "canvas.html",
         name=url_for("static", filename=filename)
     )
+
+
+@app.route("/<name>/delete", methods=["POST"])
+def delete(name):
+    filename = os.path.join("static", "imgs", name)
+    os.remove(filename)
+    return redirect(url_for("index"))
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -43,16 +54,9 @@ def index():
         if file:
             filename = file.filename
             file.save(os.path.join("static", "imgs", filename))
-            return redirect(url_for("uploaded_file", filename=filename))
-    imgs = os.listdir(r"static\imgs")
+            return redirect(request.url)
+    imgs = os.listdir(os.path.join("static", "imgs"))
     return render_template(
         "index.html",
         imgs=imgs
     )
-
-
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(
-            os.path.join("static", "imgs"), filename
-            )
